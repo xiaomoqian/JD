f0dce1716354eeec5abd68592f70c7a6548c2fdc
xiaomoqian <630841578@qq.com>
2017-11-16 11:17:26 +0800

    domain

--- yii2-app-advanced/common/config/params.php
+++ yii2-app-advanced/common/config/params.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'domain' => 'http://blog.m/',
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
